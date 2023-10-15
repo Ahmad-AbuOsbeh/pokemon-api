40989de4195d6df51a513c75987ebc06b7c5be48
Ahmad-AbuOsbeh <ahmadabuosbeh20@gmail.com>
2023-10-15 21:04:30 +0300

    create postgres database, dto and pokemon entity

--- src/pokemons/dto/create-pokemon.dto.ts
+++ src/pokemons/dto/create-pokemon.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreatePokemonDto {
+
+   @IsString()
+   readonly  name: string;
+
+   @IsNumber()
+   readonly pokdexNumer: number;
+
+   @IsNumber()
+   readonly generation: number;
+
+   @IsString()
+   readonly  type: string;
+
+   @IsString()
+   readonly  weather: string;
+};
--- src/pokemons/entities/pokemon.entity.ts
+++ src/pokemons/entities/pokemon.entity.ts
@@ -1,1 +1,34 @@
-export class Pokemon {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class Pokemon {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    pokdexNumer: number;
+
+    @Column()
+    generation: number;
+
+    @Column()
+    evolutionStage: string;
+
+    @Column()
+    evolved: number;
+
+    @Column()
+    familyID: number;
+
+    @Column()
+    type: string;
+
+    @Column()
+    weather: string;
+
+    @Column()
+    statTotal: number;
+};
